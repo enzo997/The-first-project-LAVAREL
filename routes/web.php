75859932646f1e9dc40c6efb571396727a5cd444
phamvanday <phamday999@gmail.com>
2020-03-04 10:39:05 +0700

    firstcommit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//Frontend
+Route::get('/','Homecontroller@index');
+Route::get('/home-page','Homecontroller@index');
+
+//Server admin
+Route::get('/admin','AdminController@index');
+Route::get('/dashboard','AdminController@show_Dashboard');
